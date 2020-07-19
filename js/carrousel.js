46b1d2d690efbdb8d30d0f96d865bc77f17fc402
Carolina <carolbrandes23@gmail.com>
2020-07-18 21:22:33 -0300

    estrutura html e css desktop (estático) finalizado

---
+++ js/carrousel.js
@@ -0,0 +1,54 @@
+const flktyBanner = new Flickity('.banner-slider', {
+        // options
+        pageDots: true,
+        resize: true,
+        contain: true
+});
+
+
+const flkty = new Flickity('.categorias__carrousel', {
+        // options
+        pageDots: false,
+        resize: true,
+        contain: true
+});
+
+const flkty2 = new Flickity('.mais-vendidos__carrousel', {
+        // options
+        pageDots: false,
+        resize: true,
+        contain: true,
+
+});
+
+const flkty3 = new Flickity('.melhores-ofertas__carrousel', {
+        // options
+        pageDots: false,
+        resize: true,
+        contain: true,
+
+});
+
+const flkty4 = new Flickity('.tambem-compraram__carrousel', {
+        // options
+        pageDots: false,
+        resize: true,
+        contain: true,
+
+});
+
+const flkty5 = new Flickity('.lojas-parceiras__container', {
+        // options
+        pageDots: false,
+        resize: true,
+        contain: true,
+
+});
+
+const flkty6 = new Flickity('.produtos-patrocionados__carrousel', {
+        // options
+        pageDots: false,
+        resize: true,
+        contain: true,
+
+});
