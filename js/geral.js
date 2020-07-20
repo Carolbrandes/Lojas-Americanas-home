b05a08160714eae60aeba15b92a61c51700567b6
Carolina <carolbrandes23@gmail.com>
2020-07-20 09:04:10 -0300

    finalização

--- js/geral.js
+++ js/geral.js
@@ -3,27 +3,63 @@ document.querySelector("#pesquisar-produto").value = "busque aqui seu produto";
 document.querySelector("#email").value = "Digite seu E-mail";
 
 
-// criar  o menu por departamento
+// inserir o conteudo do menu compre por departamento
 const linksMenuDepartamento = ["americanas mercado", "brinquedos e bebês", "celulares e telefonia fixa", "informáticas e tablets", "tv, áudio e home theater", "moda, beleza e perfumaria", "games, livros e filmes", "móveis e decoração", "eletrodomésticos e split", "eletroportáteis", "produtos importados", "para a sua empresa", "cartão, parceiros e serviços", "todos os departamentos"];
 
-const conteudoMenu = document.createElement('div');
-conteudoMenu.classList.add("conteudo-oculto-menu");
+const conteudoMenu = document.querySelector(".conteudo-oculto-menu");
 
-linksMenuDepartamento.map(e => {
+linksMenuDepartamento.forEach(e => {
         let a = document.createElement('a');
         a.innerHTML = e;
 
         if (e === "todos os departamentos") {
                 a.classList.add("todos-departamentos");
         }
+        a.href="#";
+
         conteudoMenu.appendChild(a);
 });
 
-document.querySelector(".menu02").appendChild(conteudoMenu);
-console.log(conteudoMenu);
-
+let contadorCliquesMenuDesk = 0;
+let contadorCliquesMenuMobile = 0;
+const setaMenu = document.querySelector(".menu-2__login-seta-exibir");
+const srcExibir = "images/seta-para-baixo-vermelha.png";
+const srcOcultar = "images/seta-para-cima-vermelha.png";
 const menuDepartamento = document.querySelector("#menu-departamento");
-menuDepartamento.addEventListener('onmouserover', () => {
-        alert("ok");
-        conteudoMenu.style.display = "block";
-});
+const menuMobile = document.querySelector(".header__menu-mobile");
+
+
+exibirMenu(menuDepartamento, conteudoMenu, contadorCliquesMenuDesk, setaMenu, srcExibir, srcOcultar);
+exibirMenu(menuMobile, conteudoMenu, contadorCliquesMenuMobile);
+
+
+function exibirMenu(elementoQueRecebeClique, elementoASerExibido, contadorCliques, seta, SrcsetaExibir, SrcsetaOcultar){
+        elementoQueRecebeClique.addEventListener("click", () => {
+                if(contadorCliques === 0 || contadorCliques % 2 === 0){
+                        exibirElemento(elementoASerExibido);
+
+                        if(seta && SrcsetaExibir && SrcsetaOcultar){
+                                alterarSetaExibicao(seta, SrcsetaOcultar);
+                        }
+                }else{
+                        ocultarElemento(elementoASerExibido);
+                        if(seta && SrcsetaExibir && SrcsetaOcultar){
+                                alterarSetaExibicao(seta, SrcsetaExibir);
+                        }
+                }
+
+                contadorCliques++;
+        });
+}
+
+function exibirElemento(elementoASerExibido){
+        elementoASerExibido.style.display = "block";
+}
+
+function ocultarElemento(elementoASerOcultado){
+        elementoASerOcultado.style.display = "none";
+}
+
+function alterarSetaExibicao(seta, src){
+        seta.src = src;
+}
